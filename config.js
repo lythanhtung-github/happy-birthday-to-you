1af02b917d983458de12b7e3b1271fdf329ae14b
lythanhtung-github <lythanhtung.work@gmail.com>
2023-10-08 23:27:11 +0700

    init

---
+++ config.js
@@ -0,0 +1,31 @@
+var config = {
+    
+    texts: [
+        "Sinh nhật vui vẻ",
+        "Mạnh mẽ lên cô gái",
+        "Dù chuyện gì xảy ra",
+        "Em vẫn không cô đơn",
+        "Vẫn có ba mẹ",
+        "Vẫn có Thầy",
+        "Vẫn có Chánh Tâm",
+        "Vẫn có bạn bè",
+        "Cố gắng lên, mọi người tin em",
+        "Khó khăn, gian nan này, chỉ thử sức của em thôi",
+        "Chaizoo",
+    ],
+    
+    imgs: {
+        "Mạnh mẽ lên cô gái": "./imgs/xiaokeai.png", 
+    },
+    
+    desc: {
+        turn_on: "Đen thui à, bật đèn lên đi!",
+        play: "Im im rứa, cho tôi con beat đê!",
+        bannar_coming: "Thêm tí màu mè nè!",
+        balloons_flying: "Cho mấy quả bong bóng bay chill chill đi!",
+        cake_fadein: "Thêm vài quả nữa =))",
+        light_candle: "Thăp nến lên mà thổi, lẹ cái tay!",
+        wish_message: "Ước lẹ ước lẹ!",
+        story: "Ước rồi thì kích tiếp mà xem",
+    }
+};
