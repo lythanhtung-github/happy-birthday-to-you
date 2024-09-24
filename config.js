9c1e34a1fec92c64d0d7ad517c769065a33b9dd3
lythanhtung-github <lythanhtung.work@gmail.com>
2024-09-24 21:26:04 +0700

    update

--- config.js
+++ config.js
@@ -2,21 +2,17 @@ var config = {
     
     texts: [
         "Sinh nhật vui vẻ",
-        "Mạnh mẽ lên cô gái",
-        "Dù chuyện gì xảy ra",
-        "Em vẫn không cô đơn",
-        "Vẫn có ba mẹ",
-        "Vẫn có Thầy",
-        "Vẫn có Chánh Tâm",
-        "Vẫn có bạn bè",
-        "Cố gắng lên, mọi người tin em",
-        "Khó khăn, gian nan này, chỉ thử sức của em thôi",
-        "Chaizoo",
+        "Sinh nhật vui vẻ",
+        "Sinh nhật vui vẻ",
+        "Sinh nhật vui vẻ",
+        "Sinh nhật vui vẻ",
+        "Sinh nhật vui vẻ",
+        "Sinh nhật vui vẻ",
     ],
     
-    imgs: {
-        "Mạnh mẽ lên cô gái": "./imgs/xiaokeai.png", 
-    },
+    // imgs: {
+    //     "Mạnh mẽ lên cô gái": "./imgs/xiaokeai.png", 
+    // },
     
     desc: {
         turn_on: "Đen thui à, bật đèn lên đi!",
